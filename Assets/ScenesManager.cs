cf5d11d8725bc0a9152566a4c5e73018aad15226
MasterSP18 <MasterSP18@gmail.com>
2017-11-13 11:34:14 -0300

    Creado SceneManager
    creado scenemanager
    -funciones para ir entre las scenes.

---
+++ Assets/ScenesManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ScenesManager {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void toMinigame(){
+		SceneManager.LoadScene ("MiniGame");
+	}
+
+	public void toMainMenu()
+	{
+		SceneManager.LoadScene ("MainMenu");
+	}
+
+	public void checkResult(bool isCorrect)
+	{
+		
+	}
+}
