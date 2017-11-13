12de73732f98db83d30272e7ef4ea8a9308f4cbd
StivArts <jaimearansoto@gmail.com>
2017-11-13 17:37:05 -0300

    Script de pieza, que contiene información sobre su forma y color

---
+++ Assets/Pieza.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pieza : MonoBehaviour
+{
+    public enum Forma
+    {
+        Triangulo, Cuadrado, Circulo, Estrella
+    }
+
+    public Forma forma;
+    public Color color;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnMouseDown()
+    {
+
+        SceneManager.Instance.showResultado((GameManager.Instance.pieza.forma = forma && GameManager.Instance.pieza.color = color));
+
+    }
+}
