62bd0e2a178e04db08b499e79864fc58babb5be2
valenzuelaElio <valenzuela.eliogd@gmail.com>
2017-11-13 17:42:38 -0300

    GameManagerActualizado2.0

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,19 +4,54 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+    GameManager Instance { get; set; }
+
     int[] Players; //Jugadores
     int jugadaIndex; //(0,5)
     bool correct;
 
-    //RecetaActual Jaime
+    private Pieza PiezaCorrecta;
 
-    public void Check(int index)
+    public void Awake()
     {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        DontDestroyOnLoad(this);
+    }
+
+
+    public void Starting()
+    {
+        ResetScores();
+        
+    }
+
+    public void Check(int index, Pieza piezaSeleccionada)
+    {
+        if (piezaSeleccionada.forma == PiezaCorrecta.forma)
+        {
+            if (piezaSeleccionada.color == PiezaCorrecta.color)
+            {
+                AddScoreToPlayer(index, 1);
+            }
+            else
+            {
+                AddScoreToPlayer(index, -1);
+            }
+        }
+        else
+        {
+            AddScoreToPlayer(index, -1);
+        }
+
 
     }
 
     void GenerarReceta()
     {
+
 
     }
 
